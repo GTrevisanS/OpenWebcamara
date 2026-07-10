7157abb530e58664c4a3aafecc65e28a22e9ab61
Guilherme de Souza Trevisan <guilherme.souza.weblinesistemas@gmail.com>
2026-07-10 16:14:20 -0300

    4ª Versão do "OpenWebcamara"

---
+++ OpenWebcamara4.0/Config.bat
@@ -0,0 +1,21 @@
+:: --------------------------- Abaixo configuro o caminho das pastas que uso
+
+
+set "MINHAPASTA=C:\Webline\"
+set "PASTAPRUDENTE=C:\Webline\"
+
+
+:: ------------------------------ Abaixo configuro o ambiente de homologação  
+
+
+set ""
+set ""
+set ""
+set ""
+
+
+:: ----- Abaixo configuro as credenciais do Python pra logar automaticamente 
+
+
+set "PYUSUARIO="
+set "PYSENHA="
