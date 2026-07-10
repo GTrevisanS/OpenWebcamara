3f9951ce59d772f6781a73105570b0d676af86cb
Guilherme de Souza Trevisan <guilherme.souza.weblinesistemas@gmail.com>
2026-07-10 16:02:14 -0300

    2ª Versão do "OpenWebCamara"

---
+++ OpenWebcamara2.0/Config.bat
@@ -0,0 +1,12 @@
+:: Abaixo configuro o caminho das pastas que uso
+
+set "MINHAPASTA=C:\Webline\MinhaPasta"
+set "PASTACLIENTE=C:\Webline\PastaCliente"
+
+
+:: Abaixo configuro o ambiente de homologação
+
+set "IP="
+set "DB="
+set "DBF="
+set "PORTA="
